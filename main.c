e49182ba32241bb18f66dd8d0a92dfa65353b352
Falzafar1 <161683671+Falzafar1@users.noreply.github.com>
2024-04-06 17:18:09 +0700

    Update main.c

--- main.c
+++ main.c
@@ -18,29 +18,39 @@
         char party[50];
         int votes;
     } Candidate;
+    
 
     // Fungsi untuk memeriksa apakah seorang kader memiliki suara lebih dari 10 %
-    int isPartyWinner(Candidate* candidates, int n, int total) {
+    int isPartyWinner(Candidate* candidates, int n, int total, char *partai) {
+        int pemenang = 0;
+        int indexpemenang = 0;
         double persen;
-
         for (int i = 0; i < n; i++){
-            persen =(double)candidates[i].votes/total;
-            if (persen > 0.1){
-                printf("%s %s %d\r\n", candidates[i].name, candidates[i].party, candidates[i].votes);
-            }    
+            if (strcmp(candidates[i].party, partai) == 0){
+                if(candidates[i].votes > pemenang){
+                    pemenang = candidates[i].votes;
+                    indexpemenang = i;
+                }
+            }
         }
+        persen =(double)candidates[indexpemenang].votes/total;
+        if (persen > 0.1){
+            printf("%s %s %d\r\n", candidates[indexpemenang].name, candidates[indexpemenang].party, candidates[indexpemenang].votes);
+        }    
         return 0;  
     }
 
     // Fungsi untuk mencari total suara 
     int findTotalPartyVotes(Candidate* candidates, int i, int total) { 
-        return total+ candidates[i].votes;
+        return total + candidates[i].votes;
     }
 
     int main() {
         int i,j,n;
+        int kondisi =0;
         int total = 0;
-        int totalelemen = 0;
+        int totalkandidat = 0;
+        int totalpartai =0;
         int VoteTertinggi = 0;
 
         // membuat input jumlah kader partai
@@ -49,35 +59,37 @@
 
         // Membuat array of struct untuk menyimpan informasi kader partai
         Candidate* candidates = (Candidate*)malloc(n * sizeof(Candidate));
+        Candidate* Partai = (Candidate*)malloc(n * sizeof(Candidate));
 
         // Membaca baris input informasi kader partai dan menyimpannya dalam array of struct
         for ( i = 0; i < n; i++){
             printf("Informasi kader ke-%d : \r\n", i+1);
             scanf("%s%s%d", candidates[i].name, candidates[i].party, &candidates[i].votes);
-            total = findTotalPartyVotes(candidates,i,total);
-            totalelemen = totalelemen +1;
-
-            for ( j = 0; j < totalelemen; j++){
-                if (strcmp(candidates[i].party, candidates[j].party) == 0){
-                    if (candidates[i].votes > VoteTertinggi){
-                        VoteTertinggi = candidates[i].votes;
-                    }
-                    if (candidates[j].votes < VoteTertinggi){
-                        strcpy(candidates[j].name, "A");
-                        strcpy(candidates[j].party, "J");
-                        candidates[j].votes = 0;
-                    }
+            totalkandidat = totalkandidat +1;
+            total = total + candidates[i].votes;
+            for ( j = 0; j < totalkandidat; j++){
+                if (strcmp(candidates[i].party, Partai[j].party) == 0){
+                    kondisi = 1;
                 }
             }
+            if (kondisi == 0){
+                totalpartai = totalpartai +1;
+                Partai = (Candidate*)realloc(Partai, totalpartai * sizeof(Candidate));
+                strcpy(Partai[totalpartai-1].party,candidates[i].party);
+                kondisi = 0;
+            }
         }
-        
         printf("\r\nAnggota Dewan Perwakilan Rakyat Anti Korupsi (DPRAK) tahun 2024\r\n");
         // Mencari kader yang lolos DPRAK
-        isPartyWinner(candidates,n,total);
+        for (i = 0; i < totalpartai; i++){
+            isPartyWinner(candidates, n, total, Partai[i].party);
+        }
+        
         printf("Semoga amanah Anti Korupsi #WakandaForever");
 
         // // // Membebaskan memori yang dialokasikan untuk array of struct
         free(candidates);
+        free(Partai);
 
         return 0;
     }
